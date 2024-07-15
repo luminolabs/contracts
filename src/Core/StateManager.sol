e32429e81227d13ce716a873d59b7aaf1cefb0f9
Shyam Patel <shyampkira@gmail.com>
2024-07-15 11:29:28 -0700

    review commit

---
+++ src/Core/StateManager.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "./storage/Constants.sol";
+
+contract StateManager is Constants {
+
+    // returns the value of current epoch
+    function getEpoch() public view returns (uint32) {
+        return (uint32(block.timestamp) / (EPOCH_LENGTH));
+    }
+
+    // returns the value of current state
+    function getState(uint8 buffer) public view returns (State) {
+        uint8 lowerLimit = buffer;
+
+        // EPOCH_LENGTH / NUM_STATES is stateLength
+        uint16 upperLimit = EPOCH_LENGTH / NUM_STATES - buffer;
+
+        if (block.timestamp % (EPOCH_LENGTH / NUM_STATES) > upperLimit || block.timestamp % (EPOCH_LENGTH / NUM_STATES) < lowerLimit) {
+            return State.Buffer;
+        }
+
+        uint8 state = uint8(((block.timestamp) / (EPOCH_LENGTH / NUM_STATES)) % (NUM_STATES));
+        return State(state);
+    }
+}
--- test/StateManager.t.sol
+++ test/StateManager.t.sol
@@ -1,78 +1,78 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// import "forge-std/Test.sol";
-// import "forge-std/console.sol";
-// import "../src/Core/StateManager.sol";
-// import "../src/Core/storage/Constants.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/Core/StateManager.sol";
+import "../src/Core/storage/Constants.sol";
 
-// contract StateManagerTest is Constants, Test {
-//     StateManager stateManager;
+contract StateManagerTest is Constants, Test {
+    StateManager stateManager;
 
-//     function setUp() public {
-//         stateManager = new StateManager();
-//     }
+    function setUp() public {
+        stateManager = new StateManager();
+    }
 
-//     function testCommitState() public {
-//         uint8 buffer = 5;
+    function testCommitState() public {
+        uint8 buffer = 5;
 
-//         vm.warp(1200);
-//         // Set block.timestamp to the start of the Commit state
-//         uint256 commitTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + 0 * (EPOCH_LENGTH / NUM_STATES) + buffer + 1;
-//         console.log(block.timestamp);
-//         console.log(commitTime);
-//         vm.warp(commitTime);
+        vm.warp(1200);
+        // Set block.timestamp to the start of the Commit state
+        uint256 commitTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + 0 * (EPOCH_LENGTH / NUM_STATES) + buffer + 1;
+        console.log(block.timestamp);
+        console.log(commitTime);
+        vm.warp(commitTime);
 
-//         State state = stateManager.getState(buffer);
-//         emit log_uint(uint8(state));
-//         assertEq(uint8(state), uint8(State.Commit));
-//     }
+        State state = stateManager.getState(buffer);
+        emit log_uint(uint8(state));
+        assertEq(uint8(state), uint8(State.Commit));
+    }
 
-//     function testRevealState() public {
-//         uint8 buffer = 5;
+    function testRevealState() public {
+        uint8 buffer = 5;
 
-//         // Set block.timestamp to the start of the Reveal state
-//         uint256 revealTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + 1 * (EPOCH_LENGTH / NUM_STATES) + buffer + 1;
-//         vm.warp(revealTime);
+        // Set block.timestamp to the start of the Reveal state
+        uint256 revealTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + 1 * (EPOCH_LENGTH / NUM_STATES) + buffer + 1;
+        vm.warp(revealTime);
 
-//         State state = stateManager.getState(buffer);
-//         emit log_uint(uint8(state));
-//         assertEq(uint8(state), uint8(State.Reveal));
-//     }
+        State state = stateManager.getState(buffer);
+        emit log_uint(uint8(state));
+        assertEq(uint8(state), uint8(State.Reveal));
+    }
 
-//     function testProposeState() public {
-//         uint8 buffer = 5;
+    function testProposeState() public {
+        uint8 buffer = 5;
 
-//         // Set block.timestamp to the start of the Propose state
-//         uint256 proposeTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + 2 * (EPOCH_LENGTH / NUM_STATES) + buffer + 1;
-//         vm.warp(proposeTime);
+        // Set block.timestamp to the start of the Propose state
+        uint256 proposeTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + 2 * (EPOCH_LENGTH / NUM_STATES) + buffer + 1;
+        vm.warp(proposeTime);
 
-//         State state = stateManager.getState(buffer);
-//         emit log_uint(uint8(state));
-//         assertEq(uint8(state), uint8(State.Propose));
-//     }
+        State state = stateManager.getState(buffer);
+        emit log_uint(uint8(state));
+        assertEq(uint8(state), uint8(State.Propose));
+    }
 
-//     function testBufferState() public {
-//         uint8 buffer = 5;
+    function testBufferState() public {
+        uint8 buffer = 5;
 
-//         // Set block.timestamp to the Buffer period at the end of the Commit state
-//         uint256 bufferTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + (EPOCH_LENGTH / NUM_STATES) + buffer - 1;
-//         vm.warp(bufferTime);
+        // Set block.timestamp to the Buffer period at the end of the Commit state
+        uint256 bufferTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + (EPOCH_LENGTH / NUM_STATES) + buffer - 1;
+        vm.warp(bufferTime);
 
-//         State state = stateManager.getState(buffer);
-//         emit log_uint(uint8(state));
-//         assertEq(uint8(state), uint8(State.Buffer));
-//     }
+        State state = stateManager.getState(buffer);
+        emit log_uint(uint8(state));
+        assertEq(uint8(state), uint8(State.Buffer));
+    }
 
-//     function testNonBufferState() public {
-//         uint8 buffer = 5;
+    function testNonBufferState() public {
+        uint8 buffer = 5;
 
-//         // Set block.timestamp to a time outside the buffer period
-//         uint256 nonBufferTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + buffer + 1;
-//         vm.warp(nonBufferTime);
+        // Set block.timestamp to a time outside the buffer period
+        uint256 nonBufferTime = block.timestamp - (block.timestamp % EPOCH_LENGTH) + buffer + 1;
+        vm.warp(nonBufferTime);
 
-//         State state = stateManager.getState(buffer);
-//         emit log_uint(uint8(state));
-//         assert(state != State.Buffer);
-//     }
-// }
+        State state = stateManager.getState(buffer);
+        emit log_uint(uint8(state));
+        assert(state != State.Buffer);
+    }
+}
