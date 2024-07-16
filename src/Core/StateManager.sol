fa2d32185fe704e13a824a81138fba6083bf75ba
Shyam Patel <shyampkira@gmail.com>
2024-07-15 17:23:14 -0700

    Added structs; added checkEpoch modifier

--- src/Core/StateManager.sol
+++ src/Core/StateManager.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.20;
 import "./storage/Constants.sol";
 
 contract StateManager is Constants {
+    
+    /**
+     * @notice a check to ensure the epoch value sent in the function is of the currect epoch
+     */
+    modifier checkEpoch(uint32 epoch) {
+        require(epoch == getEpoch(), "incorrect epoch");
+        _;
+    }
 
     // returns the value of current epoch
     function getEpoch() public view returns (uint32) {
@@ -17,11 +25,16 @@ contract StateManager is Constants {
         // EPOCH_LENGTH / NUM_STATES is stateLength
         uint16 upperLimit = EPOCH_LENGTH / NUM_STATES - buffer;
 
-        if (block.timestamp % (EPOCH_LENGTH / NUM_STATES) > upperLimit || block.timestamp % (EPOCH_LENGTH / NUM_STATES) < lowerLimit) {
+        if (
+            block.timestamp % (EPOCH_LENGTH / NUM_STATES) > upperLimit ||
+            block.timestamp % (EPOCH_LENGTH / NUM_STATES) < lowerLimit
+        ) {
             return State.Buffer;
         }
 
-        uint8 state = uint8(((block.timestamp) / (EPOCH_LENGTH / NUM_STATES)) % (NUM_STATES));
+        uint8 state = uint8(
+            ((block.timestamp) / (EPOCH_LENGTH / NUM_STATES)) % (NUM_STATES)
+        );
         return State(state);
     }
 }
