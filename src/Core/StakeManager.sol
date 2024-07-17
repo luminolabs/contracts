91fb8e4c160f270b42e9602c9cd358e6275a8273
Shyam Patel <shyampkira@gmail.com>
2024-07-16 19:22:25 -0700

    clean up, removed boilerplate files

--- src/Core/StakeManager.sol
+++ src/Core/StakeManager.sol
@@ -5,41 +5,55 @@ import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "./storage/StakeManagerStorage.sol";
 import "./StateManager.sol";
 
-
 /** @title StakeManager
  * @notice StakeManager handles stake, unstake, withdraw, reward, functions
  * for stakers
  */
 
 contract StakeManager is StakeManagerStorage, StateManager {
-   
     // IVoteManager public voteManager;
     // IERC20 public lumino;
 
-    /** 
-    *  @dev A staker can call stake() in any state
-    *  An ERC20 token($LUMINO) is staked and locked by a staker in the Contract
-    *  @param _epoch The Epoch value for which staker is requesting to stake
-    *  @param _amount The amount in LUM
-    *  @param _machineSpecInJSON JSON object for machineSpec
-    */
-   function stake(uint32 _epoch, uint256 _amount, string memory _machineSpecInJSON) external checkEpoch(_epoch) {
+    /**
+     *  @dev A staker can call stake() in any state
+     *  An ERC20 token($LUMINO) is staked and locked by a staker in the Contract
+     *  @param _epoch The Epoch value for which staker is requesting to stake
+     *  @param _amount The amount in LUM
+     *  @param _machineSpecInJSON JSON object for machineSpec
+     */
+    function stake(
+        uint32 _epoch,
+        uint256 _amount,
+        string memory _machineSpecInJSON
+    ) external checkEpoch(_epoch) {
         uint32 stakerId = stakerIds[msg.sender];
-        
+
         // first time stakers would have stakerId as 0
         if (stakerId == 0) {
-            require(_amount >= minSafeLumToken, "less than minimum safe lumino token");
+            require(
+                _amount >= minSafeLumToken,
+                "less than minimum safe lumino token"
+            );
             numStakers = numStakers + (1);
             stakerId = numStakers;
             stakerIds[msg.sender] = stakerId;
-            stakers[numStakers] = Structs.Staker(false, msg.sender, numStakers, 0, _epoch, 0, _amount, 0, _machineSpecInJSON);
+            stakers[numStakers] = Structs.Staker(
+                false,
+                msg.sender,
+                numStakers,
+                0,
+                _epoch,
+                0,
+                _amount,
+                0,
+                _machineSpecInJSON
+            );
         } else {
             require(!stakers[stakerId].isSlashed, "staker is slashed");
             stakers[stakerId].stake = stakers[stakerId].stake + (_amount);
         }
     }
 
-    
     /**
      * @dev staker must call unstake() to lock their luminoTokens
      * and should wait for params.unlock_After period
@@ -62,8 +76,7 @@ contract StakeManager is StakeManagerStorage, StateManager {
      * @param _stakerId The Id of staker
      */
     function withdraw(uint32 _stakerId) external {
-
-        uint32 epoch = getEpoch();        
+        uint32 epoch = getEpoch();
         require(_stakerId != 0, "staker doesn't exist");
 
         // Structs.Staker storage staker = stakers[_stakerId];
@@ -77,7 +90,6 @@ contract StakeManager is StakeManagerStorage, StateManager {
         _resetLock(_stakerId);
 
         // require(lumino.transfer(msg.sender, withdrawAmount));
-        
     }
 
     /**
@@ -88,7 +100,9 @@ contract StakeManager is StakeManagerStorage, StateManager {
      * @param _stakerId Id of the staker for whose lock is being reset
      */
     function _resetLock(uint32 _stakerId) private {
-        locks[stakers[_stakerId]._address] = Structs.Lock({amount: 0, unlockAfter: 0});
+        locks[stakers[_stakerId]._address] = Structs.Lock({
+            amount: 0,
+            unlockAfter: 0
+        });
     }
-
 }
