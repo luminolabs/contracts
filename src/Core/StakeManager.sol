fbf37232184d656b78cbace267fafc7cd875a384
Shyam Patel <shyampkira@gmail.com>
2024-07-16 19:04:11 -0700

    Add unstake and withdraw functionality

--- src/Core/StakeManager.sol
+++ src/Core/StakeManager.sol
@@ -20,15 +20,55 @@ contract StakeManager is StakeManagerStorage, StateManager {
     //  An ERC20 token($LUMINO) is staked and locked by a staker in the Contract
     //  param1: epoch, The Epoch value for which staker is requesting to stake
     //  param2: amount, The amount in LUM
-    function stake(uint32 epoch, uint256 amount) external checkEpoch(epoch) {
+    function stake(uint32 _epoch, uint256 _amount, string memory _machineSpecInJSON) external checkEpoch(_epoch) {
         uint32 stakerId = stakerIds[msg.sender];
         
         // first time stakers would have stakerId as 0
         if (stakerId == 0) {
-            // require();
+            require(_amount >= minSafeLumToken, "less than minimum safe lumino token");
+            numStakers = numStakers + (1);
+            stakerId = numStakers;
+            stakerIds[msg.sender] = stakerId;
+            stakers[numStakers] = Structs.Staker(false, msg.sender, numStakers, 0, _epoch, 0, _amount, 0, _machineSpecInJSON);
         } else {
-
+            require(!stakers[stakerId].isSlashed, "staker is slashed");
+            stakers[stakerId].stake = stakers[stakerId].stake + (_amount);
         }
+    }
+
+    function unstake(uint32 _stakerId, uint256 _amount) external {
+        require(_stakerId != 0, "stakerId cannot be 0");
+        require(stakers[_stakerId].stake > 0, "Non-positive stake");
+        require(locks[msg.sender].amount == 0, "Existing Unstake Lock");
+
+        uint32 epoch = getEpoch();
+
+        locks[msg.sender] = Structs.Lock(_amount, epoch + unstakeLockPeriod);
+    }
+
+    function withdraw(uint32 _stakerId) external {
+
+        uint32 epoch = getEpoch();        
+        require(_stakerId != 0, "staker doesn't exist");
+
+        Structs.Staker storage staker = stakers[_stakerId];
+        Structs.Lock storage lock = locks[msg.sender];
+
+        require(lock.unlockAfter != 0, "Did not Unstake");
+        require(lock.unlockAfter <= epoch, "Withdraw epoch not reached");
+
+        uint256 withdrawAmount = lock.amount;
+
+        _resetLock(_stakerId);
+
+        // require(lumino.transfer(msg.sender, withdrawAmount));
+        
+    }
+
+
+
+    function _resetLock(uint32 _stakerId) private {
+        locks[stakers[_stakerId]._address] = Structs.Lock({amount: 0, unlockAfter: 0});
     }
 
 }
--- src/Core/storage/StakeManagerStorage.sol
+++ src/Core/storage/StakeManagerStorage.sol
@@ -3,13 +3,36 @@ pragma solidity ^0.8.20;
 
 import "../../lib/Structs.sol";
 
-contract StakeManagerStorage is Structs {
+abstract contract StakeManagerStorage is Structs {
     // total number of staker; used as counter as well
-    uint32 public numStaker;
+    uint32 public numStakers;
 
     // mapping of stakerAddress -> stakerId
     mapping(address => uint32) public stakerIds;
 
     // mapping of stakerAddress -> stakerId
     mapping(uint32 => Structs.Staker) public stakers;
+
+    // mapping of stakerAddress -> LockInfo
+    mapping(address => Structs.Lock) public locks;
+
+    struct SlashNums {
+        // percent bounty from staker's stake to be received by the bounty hunter
+        uint32 bounty;
+        // percent RAZOR burn from staker's stake
+        uint32 burn;
+        // percent from staker's stake to be kept by staker
+        uint32 keep;
+    }
+
+    // minimum amount of stake required to become a staker
+    uint256 public minStake = 20000 * (10**18);
+
+    // minimum amount of stake required to become a staker
+    uint256 public minSafeLumToken = 10000 * (10**18);
+
+    uint8 public buffer = 5;
+    // the number of epochs for which the stake is locked for calling unstake()
+    uint16 public unstakeLockPeriod = 1;
+
 }
