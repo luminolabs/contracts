16626edd8ce9ca2c57f878c3da857d4fcf835d38
Shyam Patel <shyampkira@gmail.com>
2024-07-16 19:16:37 -0700

    Add description for stakeManager functionalities

--- src/Core/StakeManager.sol
+++ src/Core/StakeManager.sol
@@ -16,11 +16,14 @@ contract StakeManager is StakeManagerStorage, StateManager {
     // IVoteManager public voteManager;
     // IERC20 public lumino;
 
-    //  A staker can stake in any state
-    //  An ERC20 token($LUMINO) is staked and locked by a staker in the Contract
-    //  param1: epoch, The Epoch value for which staker is requesting to stake
-    //  param2: amount, The amount in LUM
-    function stake(uint32 _epoch, uint256 _amount, string memory _machineSpecInJSON) external checkEpoch(_epoch) {
+    /** 
+    *  @dev A staker can call stake() in any state
+    *  An ERC20 token($LUMINO) is staked and locked by a staker in the Contract
+    *  @param _epoch The Epoch value for which staker is requesting to stake
+    *  @param _amount The amount in LUM
+    *  @param _machineSpecInJSON JSON object for machineSpec
+    */
+   function stake(uint32 _epoch, uint256 _amount, string memory _machineSpecInJSON) external checkEpoch(_epoch) {
         uint32 stakerId = stakerIds[msg.sender];
         
         // first time stakers would have stakerId as 0
@@ -36,6 +39,14 @@ contract StakeManager is StakeManagerStorage, StateManager {
         }
     }
 
+    
+    /**
+     * @dev staker must call unstake() to lock their luminoTokens
+     * and should wait for params.unlock_After period
+     * after which he/she can call Withdraw() after unstakeLockPeriod
+     * @param _stakerId The Id of staker associated with sRZR which user want to unstake
+     * @param _amount The Amount in sRZR
+     */
     function unstake(uint32 _stakerId, uint256 _amount) external {
         require(_stakerId != 0, "stakerId cannot be 0");
         require(stakers[_stakerId].stake > 0, "Non-positive stake");
@@ -46,18 +57,22 @@ contract StakeManager is StakeManagerStorage, StateManager {
         locks[msg.sender] = Structs.Lock(_amount, epoch + unstakeLockPeriod);
     }
 
+    /**
+     * @notice staker can claim their locked $LUMINO tokens.
+     * @param _stakerId The Id of staker
+     */
     function withdraw(uint32 _stakerId) external {
 
         uint32 epoch = getEpoch();        
         require(_stakerId != 0, "staker doesn't exist");
 
-        Structs.Staker storage staker = stakers[_stakerId];
+        // Structs.Staker storage staker = stakers[_stakerId];
         Structs.Lock storage lock = locks[msg.sender];
 
         require(lock.unlockAfter != 0, "Did not Unstake");
         require(lock.unlockAfter <= epoch, "Withdraw epoch not reached");
 
-        uint256 withdrawAmount = lock.amount;
+        // uint256 withdrawAmount = lock.amount;
 
         _resetLock(_stakerId);
 
@@ -65,8 +80,13 @@ contract StakeManager is StakeManagerStorage, StateManager {
         
     }
 
-
-
+    /**
+     * @notice a private function being called when the staker
+     * successfully withdraws his funds from the network. This is
+     * being done so that the staker can unstake and withdraw his remaining funds
+     * incase of partial unstake
+     * @param _stakerId Id of the staker for whose lock is being reset
+     */
     function _resetLock(uint32 _stakerId) private {
         locks[stakers[_stakerId]._address] = Structs.Lock({amount: 0, unlockAfter: 0});
     }
