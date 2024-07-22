72fbf050b0743df949a79a275179e6045c81449e
Shyam Patel <shyampkira@gmail.com>
2024-07-22 14:50:23 -0700

    ADd reveal function description

--- src/Core/VoteManager.sol
+++ src/Core/VoteManager.sol
@@ -6,6 +6,7 @@ import "./ACL.sol";
 import "./storage/VoteManagerStorage.sol";
 import "./StateManager.sol";
 import "./StakeManager.sol";
+import "./interface/IStakeManager.sol";
 import "../Initializable.sol";
 
 /** @title VoteManager
@@ -16,14 +17,13 @@ import "../Initializable.sol";
 contract VoteManager is Initializable, VoteManagerStorage, StateManager, ACL {
     // IStakeManager public stakeManager;
     // IERC20 public lumino;
+    IStakeManager public stakeManager;
 
     function initialize(
         address stakeManagerAddress,
-        address rewardManagerAddress,
         address blockManagerAddress
     ) external initializer hasRole(DEFAULT_ADMIN_ROLE, msg.sender) {
-        // stakeManager = IStakeManager(stakeManagerAddress);
-        // rewardManager = IRewardManager(rewardManagerAddress);
+        stakeManager = IStakeManager(stakeManagerAddress);
         // blockManager = IBlockManager(blockManagerAddress);
     }
 
@@ -43,7 +43,7 @@ contract VoteManager is Initializable, VoteManagerStorage, StateManager, ACL {
     function commit(
         uint32 epoch,
         bytes32 commitment
-    ) external checkEpochAndState(State.Commit, epoch) {
+    ) external checkEpochAndState(State.Commit, epoch, buffer) {
         require(commitment != 0x0, "Invalid commitment");
         uint32 stakerId = stakeManager.getStakerId(msg.sender);
         require(
@@ -63,5 +63,27 @@ contract VoteManager is Initializable, VoteManagerStorage, StateManager, ACL {
             commitments[stakerId].epoch = epoch;
             commitments[stakerId].commitmentHash = commitment;
         }
+    }
+
+    /**
+     * @notice staker reveal the jobs that they had committed to the protocol in the commit state.
+     * Stakers would only reveal the jobs they have been allocated
+     * @dev stakers would need to submit their commitments/votes in accordance of how they were assigned to the staker.
+     * for example, if they are assigned the following job ids: [2,5,4], they would to send their reveal votes in the same order only
+     * // The votes of other ids dont matter but they should not be passed in the values.
+     * So staker would have to pass the proof path of the assigned values of the merkle tree, root of the merkle tree and
+     * the values being revealed into a struct in the Structs.MerkleTree format.
+     * @param epoch epoch when the revealed their votes
+     * @param tree the merkle tree struct of the staker
+     * @param signature staker's signature on the messageHash which calculates
+     * the secret using which seed would be calculated and thereby checking for collection allocation
+     */
+    function reveal(
+        uint32 epoch,
+        Structs.MerkleTree memory tree,
+        bytes memory signature
+    ) external initialized checkEpochAndState(State.Reveal, epoch, buffer) {
+        uint32 stakerId = stakeManager.getStakerId(msg.sender);
+        
     }
 }
