27a1f4ceaf3344890a20d65d8468529c3cb57f77
Shyam Patel <shyampkira@gmail.com>
2024-07-29 15:04:30 -0700

    Add intializer to ACL contract

--- src/Core/ACL.sol
+++ src/Core/ACL.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "../../lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+import "../Initializable.sol";
 
-contract ACL is AccessControl {
+contract ACL is AccessControl, Initializable {
     //  the deployer of the network is given to the default admin
     //  role which gives other roles to contracts
-    constructor() {
+    function initialize() public initializer {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 }
