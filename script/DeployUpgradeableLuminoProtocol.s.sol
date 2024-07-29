4b10072247d35a75d8241d002e5b524bafeda7cb
Shyam Patel <shyampkira@gmail.com>
2024-07-29 15:40:19 -0700

    fix deployment bugs

--- script/DeployUpgradeableLuminoProtocol.s.sol
+++ script/DeployUpgradeableLuminoProtocol.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-
 import "../lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "../lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
 import "../src/Core/ACL.sol";
@@ -12,65 +11,88 @@ import "../src/Core/VoteManager.sol";
 import "../src/Core/BlockManager.sol";
 
 contract DeployUpgradeableLuminoProtocol is Script {
+    ProxyAdmin public proxyAdmin;
+    ACL public aclImpl;
+    StakeManager public stakeManagerImpl;
+    JobsManager public jobsManagerImpl;
+    VoteManager public voteManagerImpl;
+    BlockManager public blockManagerImpl;
+
+    TransparentUpgradeableProxy public aclProxy;
+    TransparentUpgradeableProxy public stakeManagerProxy;
+    TransparentUpgradeableProxy public jobsManagerProxy;
+    TransparentUpgradeableProxy public voteManagerProxy;
+    TransparentUpgradeableProxy public blockManagerProxy;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy ProxyAdmin
-        ProxyAdmin proxyAdmin = new ProxyAdmin(msg.sender);
+        deployContracts(deployer);
+        initializeContracts(deployer);
+        setupRoles(deployer);
 
-        // Deploy implementation contracts
-        ACL aclImpl = new ACL();
-        StakeManager stakeManagerImpl = new StakeManager();
-        JobsManager jobsManagerImpl = new JobsManager();
-        VoteManager voteManagerImpl = new VoteManager();
-        BlockManager blockManagerImpl = new BlockManager();
+        vm.stopBroadcast();
 
-        // Deploy proxies
+        logAddresses();
+    }
+
+    function deployContracts(address deployer) internal {
+        proxyAdmin = new ProxyAdmin(deployer);
+
+        aclImpl = new ACL();
+        stakeManagerImpl = new StakeManager();
+        jobsManagerImpl = new JobsManager();
+        voteManagerImpl = new VoteManager();
+        blockManagerImpl = new BlockManager();
+
         bytes memory emptyData = "";
-        TransparentUpgradeableProxy aclProxy = new TransparentUpgradeableProxy(
+        aclProxy = new TransparentUpgradeableProxy(
             address(aclImpl),
             address(proxyAdmin),
             emptyData
         );
-        TransparentUpgradeableProxy stakeManagerProxy = new TransparentUpgradeableProxy(
+        stakeManagerProxy = new TransparentUpgradeableProxy(
             address(stakeManagerImpl),
             address(proxyAdmin),
             emptyData
         );
-        TransparentUpgradeableProxy jobsManagerProxy = new TransparentUpgradeableProxy(
+        jobsManagerProxy = new TransparentUpgradeableProxy(
             address(jobsManagerImpl),
             address(proxyAdmin),
             emptyData
         );
-        TransparentUpgradeableProxy voteManagerProxy = new TransparentUpgradeableProxy(
+        voteManagerProxy = new TransparentUpgradeableProxy(
             address(voteManagerImpl),
             address(proxyAdmin),
             emptyData
         );
-        TransparentUpgradeableProxy blockManagerProxy = new TransparentUpgradeableProxy(
+        blockManagerProxy = new TransparentUpgradeableProxy(
             address(blockManagerImpl),
             address(proxyAdmin),
             emptyData
         );
+    }
 
-        // Initialize contracts through proxies
+    function initializeContracts(address deployer) internal {
         ACL(address(aclProxy)).initialize();
-        StakeManager(address(stakeManagerProxy)).initialize(/* Add necessary parameters */);
-        JobsManager(address(jobsManagerProxy)).initialize(5); // Assuming 5 jobs per staker, adjust as needed
+        StakeManager(address(stakeManagerProxy)).initialize();
+        JobsManager(address(jobsManagerProxy)).initialize(5);
         VoteManager(address(voteManagerProxy)).initialize(address(stakeManagerProxy), address(jobsManagerProxy));
         BlockManager(address(blockManagerProxy)).initialize(address(stakeManagerProxy), address(jobsManagerProxy), address(voteManagerProxy), 1000 ether);
+    }
 
-        // Set up roles and permissions
+    function setupRoles(address deployer) internal {
         bytes32 adminRole = ACL(address(aclProxy)).DEFAULT_ADMIN_ROLE();
+        ACL(address(aclProxy)).grantRole(adminRole, deployer);
         ACL(address(aclProxy)).grantRole(adminRole, address(stakeManagerProxy));
         ACL(address(aclProxy)).grantRole(adminRole, address(jobsManagerProxy));
         ACL(address(aclProxy)).grantRole(adminRole, address(voteManagerProxy));
         ACL(address(aclProxy)).grantRole(adminRole, address(blockManagerProxy));
+    }
 
-        vm.stopBroadcast();
-
-        // Log deployed addresses
+    function logAddresses() internal view {
         console.log("ProxyAdmin deployed at:", address(proxyAdmin));
         console.log("ACL Proxy deployed at:", address(aclProxy));
         console.log("StakeManager Proxy deployed at:", address(stakeManagerProxy));
