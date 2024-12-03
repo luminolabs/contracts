6e97a14a8d8bd9a25365af698adb68ff466b7ce9
Shyam Patel <shyampkira@gmail.com>
2024-12-03 23:59:00 +0530

    differentiate roles from single admin role

--- src/Core/storage/Constants.sol
+++ src/Core/storage/Constants.sol
@@ -60,4 +60,31 @@ contract Constants {
      * @notice Number of epochs for which stake is locked after calling unstake()
      */
     uint16 public unstakeLockPeriod = 1;
+
+bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
+    bytes32 public constant PAUSE_ROLE = keccak256("PAUSE_ROLE");
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+    
+    // Staking Related Roles
+    bytes32 public constant STAKE_MANAGER_ROLE = keccak256("STAKE_MANAGER_ROLE");
+    bytes32 public constant SLASHER_ROLE = keccak256("SLASHER_ROLE");
+    bytes32 public constant REWARD_DISTRIBUTOR_ROLE = keccak256("REWARD_DISTRIBUTOR_ROLE");
+    
+    // Job Management Roles
+    bytes32 public constant JOB_CREATOR_ROLE = keccak256("JOB_CREATOR_ROLE");
+    bytes32 public constant JOB_ASSIGNER_ROLE = keccak256("JOB_ASSIGNER_ROLE");
+    bytes32 public constant JOB_VALIDATOR_ROLE = keccak256("JOB_VALIDATOR_ROLE");
+    
+    // TODO: Block Management Roles for
+    // bytes32 public constant BLOCK_PROPOSER_ROLE = keccak256("BLOCK_PROPOSER_ROLE");
+    // bytes32 public constant BLOCK_VALIDATOR_ROLE = keccak256("BLOCK_VALIDATOR_ROLE");
+    
+    // Parameter Management Roles
+    bytes32 public constant FEE_MANAGER_ROLE = keccak256("FEE_MANAGER_ROLE");
+    
+    // Special Access Roles
+    bytes32 public constant GOVERNANCE_ROLE = keccak256("GOVERNANCE_ROLE");
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+
 }
