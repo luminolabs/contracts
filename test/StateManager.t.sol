6747d38e0089ca081bdad9e7d8e877bd2a65ede1
kira <shyampkira@gmail.com>
2025-01-08 22:02:09 -0800

    [REVIEW NOT REQUIRED] Update Develop with current Head (#26)
    * update .gitignore for repomix
    
    * connect stakeManager and JobsManager to use proxy addresses
    
    * Update deployment script
    
    * differentiate roles from single admin role
    
    * nit: spacing
    
    * Add github actions config for the repo
    
    * Update .gitignore
    
    * add prettier config; package contract in yarn
    
    * run CI on PR
    
    * rename the repo secret
    
    * Update Constants.sol
    
    * fix testcases for jobs and stake manager
    
    * keep version to prevent warning and build error
    
    * Update solidity version
    
    * add slither report to .gitignore
    
    * run forge build before running slither checks
    
    * Add deployment to the pipeline
    
    * add remappings for correctly using imports
    
    * Update imports to not use relative path
    
    * Update imports to not use relative path
    
    * Update gitmodules and method for dependency installation
    
    * remove default module
    
    * fix testcases that were broken
    
    * Add gas snapshot; update config
    
    * don't exit procses on snapshot
    
    * remove codecov app
    
    * remove --use solc; update .gitignore
    
    * fix indent
    
    * remove debug flag
    
    * nit: stlye
    
    * remove codecov app
    
    * Add remappings.txt
    
    * Update upgradable contracts version
    
    * remove remappings forge
    
    * reinitialize mappings
    
    * remove uploading sarif file
    
    * remove lib from git repo
    
    * add lib to repo
    
    * reset readme
    
    * remove initial files
    
    * remove initial files
    
    * Add unit tests for ACL.sol
    
    * Increase stateManager coverage to 100%
    
    * Increase jobsManager UT coverage
    
    * fix failing testcase for stateManager
    
    * Increase stakeManager UT coverage
    
    * restrict initializer's visibility to view
    
    * skip deployment script in UT coverage
    
    * revert overwritten changes

--- test/StateManager.t.sol
+++ test/StateManager.t.sol
@@ -6,60 +6,163 @@ import "forge-std/console.sol";
 import "../src/Core/StateManager.sol";
 import "../src/Core/storage/Constants.sol";
 
-contract StateManagerTest is Constants, Test {
-    StateManager stateManager;
+contract StateManagerTest is Test, Constants, StateManager {
+    StateManager public stateManager;
+    uint8 public constant BUFFER = 5;
 
     function setUp() public {
         stateManager = new StateManager();
     }
 
-    function testCommitState() public {
-        uint8 buffer = 5;
-        // Set block.timestamp to be in the Assign state window
-        uint256 assignTime = (EPOCH_LENGTH / NUM_STATES) / 2; // Middle of Assign state
+    function testGetEpoch() public {
+        // Test at epoch 0
+        assertEq(stateManager.getEpoch(), 0);
+
+        // Test at epoch 1
+        vm.warp(EPOCH_LENGTH);
+        assertEq(stateManager.getEpoch(), 1);
+
+        // Test at epoch 5
+        vm.warp(5 * EPOCH_LENGTH);
+        assertEq(stateManager.getEpoch(), 5);
+
+        // Test at a specific timestamp
+        uint256 timestamp = 1703347200; // Some specific timestamp
+        vm.warp(timestamp);
+        assertEq(stateManager.getEpoch(), timestamp / EPOCH_LENGTH);
+    }
+
+    function testAssignState() public {
+        uint256 stateLength = EPOCH_LENGTH / NUM_STATES;
+        
+        // Test middle of Assign state
+        uint256 assignTime = stateLength / 2;
         vm.warp(assignTime);
-        
-        State state = stateManager.getState(buffer);
-        assertEq(uint8(state), uint8(State.Assign));
+        assertEq(uint8(stateManager.getState(BUFFER)), uint8(State.Assign));
     }
 
-    function testRevealState() public {
-        uint8 buffer = 5;
-        // Set block.timestamp to be in the Update state window
-        uint256 updateTime = (EPOCH_LENGTH / NUM_STATES) + (EPOCH_LENGTH / NUM_STATES / 2); // Middle of Update state
+    function testUpdateState() public {
+        uint256 stateLength = EPOCH_LENGTH / NUM_STATES;
+        
+        // Test middle of Update state
+        uint256 updateTime = stateLength + (stateLength / 2);
         vm.warp(updateTime);
-        
-        State state = stateManager.getState(buffer);
-        assertEq(uint8(state), uint8(State.Update));
+        assertEq(uint8(stateManager.getState(BUFFER)), uint8(State.Update));
     }
 
-    function testProposeState() public {
-        uint8 buffer = 5;
-        // Set block.timestamp to be in the Confirm state window
-        uint256 confirmTime = 2 * (EPOCH_LENGTH / NUM_STATES) + (EPOCH_LENGTH / NUM_STATES / 2); // Middle of Confirm state
-        vm.warp(confirmTime);
+    function testConfirmState() public {
+        uint256 stateLength = EPOCH_LENGTH / NUM_STATES;
         
-        State state = stateManager.getState(buffer);
-        assertEq(uint8(state), uint8(State.Confirm));
+        // Test middle of Confirm state
+        uint256 confirmTime = 2 * stateLength + (stateLength / 2);
+        vm.warp(confirmTime);
+        assertEq(uint8(stateManager.getState(BUFFER)), uint8(State.Confirm));
     }
 
     function testBufferState() public {
-        uint8 buffer = 5;
-        // Set block.timestamp to be in the buffer period
-        uint256 bufferTime = (EPOCH_LENGTH / NUM_STATES) - 2; // Just before state transition
-        vm.warp(bufferTime);
+        uint256 stateLength = EPOCH_LENGTH / NUM_STATES;
         
-        State state = stateManager.getState(buffer);
-        assertEq(uint8(state), uint8(State.Buffer));
+        // Test at the start buffer
+        vm.warp(2); // Just after epoch start
+        assertEq(uint8(stateManager.getState(BUFFER)), uint8(State.Buffer));
+
+        // Test at the end buffer of first state
+        vm.warp(stateLength - 2);
+        assertEq(uint8(stateManager.getState(BUFFER)), uint8(State.Buffer));
+
+        // Test at the buffer between states
+        vm.warp(stateLength + 2);
+        assertEq(uint8(stateManager.getState(BUFFER)), uint8(State.Buffer));
     }
 
-    function testNonBufferState() public {
-        uint8 buffer = 5;
-        // Set block.timestamp to be clearly in a non-buffer period
-        uint256 nonBufferTime = (EPOCH_LENGTH / NUM_STATES) / 2; // Middle of first state
-        vm.warp(nonBufferTime);
+    function testStateModifier() public {
+        function(State, uint8) external returns (bool) fn = this.dummyStateFunction;
         
-        State state = stateManager.getState(buffer);
-        assertTrue(state != State.Buffer);
+        // Test happy path
+        uint256 stateLength = EPOCH_LENGTH / NUM_STATES;
+        uint256 assignTime = stateLength / 2;
+        vm.warp(assignTime);
+        assertTrue(fn(State.Assign, BUFFER));
+
+        // Test incorrect state
+        vm.expectRevert("Incorrect state");
+        fn(State.Update, BUFFER);
+    }
+
+    function testEpochModifier() public {
+        function(uint32) external returns (bool) fn = this.dummyEpochFunction;
+        
+        // Test happy path
+        assertTrue(fn(0));
+
+        // Test incorrect epoch
+        vm.warp(EPOCH_LENGTH); // Move to epoch 1
+        vm.expectRevert("Incorrect epoch");
+        fn(0);
+    }
+
+    function testEpochAndStateModifier() public {
+        function(State, uint32, uint8) external returns (bool) fn = this.dummyEpochAndStateFunction;
+        
+        // Test happy path
+        uint256 stateLength = EPOCH_LENGTH / NUM_STATES;
+        uint256 assignTime = stateLength / 2;
+        vm.warp(assignTime);
+        assertTrue(fn(State.Assign, 0, BUFFER));
+
+        // Test incorrect epoch
+        vm.warp(EPOCH_LENGTH); // Move to epoch 1
+        vm.expectRevert("Incorrect epoch");
+        fn(State.Assign, 0, BUFFER);
+
+        // Test incorrect state
+        vm.warp(assignTime);
+        vm.expectRevert("Incorrect state");
+        fn(State.Update, 0, BUFFER);
+    }
+
+    // Dummy functions to test modifiers
+    function dummyStateFunction(State state, uint8 buffer) external view checkState(state, buffer) returns (bool) {
+        return true;
+    }
+
+    function dummyEpochFunction(uint32 epoch) external view checkEpoch(epoch) returns (bool) {
+        return true;
+    }
+
+    function dummyEpochAndStateFunction(State state, uint32 epoch, uint8 buffer) 
+        external 
+        view 
+        checkEpochAndState(state, epoch, buffer) 
+        returns (bool) 
+    {
+        return true;
+    }
+
+    function testStateTransitions() public {
+        uint256 stateLength = EPOCH_LENGTH / NUM_STATES;
+        
+        // Test each state in sequence
+        for (uint8 i = 0; i < NUM_STATES; i++) {
+            // Start of state (plus buffer to avoid being in buffer state)
+            uint256 stateStart = i * stateLength;
+            vm.warp(stateStart + BUFFER + 1);
+            
+            State currentState = stateManager.getState(BUFFER);
+            console.log("Testing state transition. Expected:", i, "Got:", uint(currentState));
+            assertEq(uint(currentState), uint(i), string(abi.encodePacked("State should be ", vm.toString(i))));
+
+            // Middle of state
+            vm.warp(stateStart + (stateLength / 2));
+            assertEq(uint(stateManager.getState(BUFFER)), uint(i), "Middle of state should match");
+
+            // Just before buffer (should still be in current state)
+            vm.warp(stateStart + stateLength - BUFFER - 1);
+            assertEq(uint(stateManager.getState(BUFFER)), uint(i), "Before buffer should match");
+
+            // In buffer period
+            vm.warp(stateStart + stateLength - (BUFFER / 2));
+            assertEq(uint(stateManager.getState(BUFFER)), uint(State.Buffer), "Should be in buffer state");
+        }
     }
 }
