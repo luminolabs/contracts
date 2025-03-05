43c1a4eb3cc5fc1c5f8360636d28428d54f7bbe4
Shyam Patel <shyampkira@gmail.com>
2025-03-05 11:38:24 -0800

    Update the deploy script to support proxy deployments

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,7 +7,8 @@ import {StdChains} from "../lib/forge-std/src/StdChains.sol";
 import {StdCheatsSafe} from "../lib/forge-std/src/StdCheats.sol";
 import {StdUtils} from "../lib/forge-std/src/StdUtils.sol";
 import {console} from "../lib/forge-std/src/console.sol";
-
+import {TransparentUpgradeableProxy} from "../lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ProxyAdmin} from "../lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
 import {AccessManager} from "../src/AccessManager.sol";
 import {EpochManager} from "../src/EpochManager.sol";
 import {IncentiveManager} from "../src/IncentiveManager.sol";
@@ -21,17 +22,32 @@ import {WhitelistManager} from "../src/WhitelistManager.sol";
 import {LShared} from "../src/libraries/LShared.sol";
 
 contract DeploymentScript is Script {
-    // Contract instances
-    LuminoToken public token;
-    AccessManager public accessManager;
-    WhitelistManager public whitelistManager;
-    EpochManager public epochManager;
-    NodeEscrow public nodeEscrow;
-    NodeManager public nodeManager;
-    JobEscrow public jobEscrow;
-    JobManager public jobManager;
-    LeaderManager public leaderManager;
-    IncentiveManager public incentiveManager;
+    // Contract implementations
+    LuminoToken public tokenImpl;
+    AccessManager public accessManagerImpl;
+    WhitelistManager public whitelistManagerImpl;
+    EpochManager public epochManagerImpl;
+    NodeEscrow public nodeEscrowImpl;
+    NodeManager public nodeManagerImpl;
+    JobEscrow public jobEscrowImpl;
+    JobManager public jobManagerImpl;
+    LeaderManager public leaderManagerImpl;
+    IncentiveManager public incentiveManagerImpl;
+
+    // Proxy admin
+    ProxyAdmin public proxyAdmin;
+
+    // Proxies (the actual contract addresses users will interact with)
+    address public tokenProxy;
+    address public accessManagerProxy;
+    address public whitelistManagerProxy;
+    address public epochManagerProxy;
+    address public nodeEscrowProxy;
+    address public nodeManagerProxy;
+    address public jobEscrowProxy;
+    address public jobManagerProxy;
+    address public leaderManagerProxy;
+    address public incentiveManagerProxy;
 
     function run() external {
         // Get deployment private key from environment
@@ -40,66 +56,133 @@ contract DeploymentScript is Script {
         // Start broadcast
         vm.startBroadcast(deployerPrivateKey);
 
-        // 1. Deploy core contracts
-        token = new LuminoToken();
-        accessManager = new AccessManager();
+        // Deploy ProxyAdmin first - this will manage all proxies
+        proxyAdmin = new ProxyAdmin(msg.sender);
+        console.log("ProxyAdmin:", address(proxyAdmin));
 
-        // 2. Deploy contracts with single dependencies
-        whitelistManager = new WhitelistManager(
-            address(accessManager)
+        // 1. Deploy implementations
+        tokenImpl = new LuminoToken();
+        accessManagerImpl = new AccessManager();
+        whitelistManagerImpl = new WhitelistManager();
+        epochManagerImpl = new EpochManager();
+        nodeEscrowImpl = new NodeEscrow();
+        jobEscrowImpl = new JobEscrow();
+        nodeManagerImpl = new NodeManager();
+        leaderManagerImpl = new LeaderManager();
+        incentiveManagerImpl = new IncentiveManager();
+
+        // 2. Deploy and initialize proxies
+
+        // LuminoToken proxy
+        tokenProxy = deployProxy(
+            address(tokenImpl),
+            abi.encodeWithSelector(
+                LuminoToken.initialize.selector
+            )
         );
 
-        epochManager = new EpochManager();
-
-        // 3. Deploy escrow contracts
-        nodeEscrow = new NodeEscrow(
-            address(accessManager),
-            address(token)
+        // AccessManager proxy
+        accessManagerProxy = deployProxy(
+            address(accessManagerImpl),
+            abi.encodeWithSelector(
+                AccessManager.initialize.selector
+            )
         );
 
-        jobEscrow = new JobEscrow(
-            address(accessManager),
-            address(token)
+        // WhitelistManager proxy
+        whitelistManagerProxy = deployProxy(
+            address(whitelistManagerImpl),
+            abi.encodeWithSelector(
+                WhitelistManager.initialize.selector,
+                accessManagerProxy
+            )
         );
 
-        // 4. Deploy node and job management
-        nodeManager = new NodeManager(
-            address(nodeEscrow),
-            address(whitelistManager),
-            address(accessManager)
+        // EpochManager proxy
+        epochManagerProxy = deployProxy(
+            address(epochManagerImpl),
+            abi.encodeWithSelector(
+                EpochManager.initialize.selector
+            )
         );
 
-        // 5. Deploy leader manager
-        leaderManager = new LeaderManager(
-            address(epochManager),
-            address(nodeManager),
-            address(nodeEscrow),
-            address(accessManager),
-            address(whitelistManager)
+        // NodeEscrow proxy
+        nodeEscrowProxy = deployProxy(
+            address(nodeEscrowImpl),
+            abi.encodeWithSelector(
+                NodeEscrow.initialize.selector,
+                accessManagerProxy,
+                tokenProxy
+            )
         );
 
-        // 6. Deploy job manager after leader manager
-        jobManager = new JobManager(
-            address(nodeManager),
-            address(leaderManager),
-            address(epochManager),
-            address(jobEscrow),
-            address(accessManager)
+        // JobEscrow proxy
+        jobEscrowProxy = deployProxy(
+            address(jobEscrowImpl),
+            abi.encodeWithSelector(
+                JobEscrow.initialize.selector,
+                accessManagerProxy,
+                tokenProxy
+            )
         );
 
-        // 7. Finally deploy incentive manager
-        incentiveManager = new IncentiveManager(
-            address(epochManager),
-            address(leaderManager),
-            address(jobManager),
-            address(nodeManager),
-            address(nodeEscrow)
+        // NodeManager proxy
+        nodeManagerProxy = deployProxy(
+            address(nodeManagerImpl),
+            abi.encodeWithSelector(
+                NodeManager.initialize.selector,
+                nodeEscrowProxy,
+                whitelistManagerProxy,
+                accessManagerProxy
+            )
         );
 
-        // 8. Set up roles
+        // LeaderManager proxy
+        leaderManagerProxy = deployProxy(
+            address(leaderManagerImpl),
+            abi.encodeWithSelector(
+                LeaderManager.initialize.selector,
+                epochManagerProxy,
+                nodeManagerProxy,
+                nodeEscrowProxy,
+                accessManagerProxy,
+                whitelistManagerProxy
+            )
+        );
+
+        // JobManager proxy
+        jobManagerProxy = deployProxy(
+            address(jobManagerImpl),
+            abi.encodeWithSelector(
+                JobManager.initialize.selector,
+                nodeManagerProxy,
+                leaderManagerProxy,
+                epochManagerProxy,
+                jobEscrowProxy,
+                accessManagerProxy
+            )
+        );
+
+        // IncentiveManager proxy
+        incentiveManagerProxy = deployProxy(
+            address(incentiveManagerImpl),
+            abi.encodeWithSelector(
+                IncentiveManager.initialize.selector,
+                epochManagerProxy,
+                leaderManagerProxy,
+                jobManagerProxy,
+                nodeManagerProxy,
+                nodeEscrowProxy
+            )
+        );
+
+        // 3. Set up roles
+        // Create interface instances to interact with the proxies
+        AccessManager accessManager = AccessManager(accessManagerProxy);
+
         // Grant CONTRACTS_ROLE to contracts that need it
-        accessManager.grantRole(LShared.CONTRACTS_ROLE, address(incentiveManager));
-        accessManager.grantRole(LShared.CONTRACTS_ROLE, address(jobManager));
+        accessManager.grantRole(LShared.CONTRACTS_ROLE, incentiveManagerProxy);
+        accessManager.grantRole(LShared.CONTRACTS_ROLE, jobManagerProxy);
 
         // Grant OPERATOR_ROLE to deployer
         accessManager.grantRole(LShared.OPERATOR_ROLE, msg.sender);
@@ -109,15 +192,35 @@ contract DeploymentScript is Script {
 
         // Log deployed addresses
         console.log("Deployment completed. Contract addresses:");
-        console.log("LuminoToken:", address(token));
-        console.log("AccessManager:", address(accessManager));
-        console.log("WhitelistManager:", address(whitelistManager));
-        console.log("EpochManager:", address(epochManager));
-        console.log("NodeEscrow:", address(nodeEscrow));
-        console.log("JobEscrow:", address(jobEscrow));
-        console.log("NodeManager:", address(nodeManager));
-        console.log("JobManager:", address(jobManager));
-        console.log("LeaderManager:", address(leaderManager));
-        console.log("IncentiveManager:", address(incentiveManager));
+        console.log("LuminoToken (Implementation):", address(tokenImpl));
+        console.log("LuminoToken (Proxy):", tokenProxy);
+        console.log("AccessManager (Implementation):", address(accessManagerImpl));
+        console.log("AccessManager (Proxy):", accessManagerProxy);
+        console.log("WhitelistManager (Implementation):", address(whitelistManagerImpl));
+        console.log("WhitelistManager (Proxy):", whitelistManagerProxy);
+        console.log("EpochManager (Implementation):", address(epochManagerImpl));
+        console.log("EpochManager (Proxy):", epochManagerProxy);
+        console.log("NodeEscrow (Implementation):", address(nodeEscrowImpl));
+        console.log("NodeEscrow (Proxy):", nodeEscrowProxy);
+        console.log("JobEscrow (Implementation):", address(jobEscrowImpl));
+        console.log("JobEscrow (Proxy):", jobEscrowProxy);
+        console.log("NodeManager (Implementation):", address(nodeManagerImpl));
+        console.log("NodeManager (Proxy):", nodeManagerProxy);
+        console.log("JobManager (Implementation):", address(jobManagerImpl));
+        console.log("JobManager (Proxy):", jobManagerProxy);
+        console.log("LeaderManager (Implementation):", address(leaderManagerImpl));
+        console.log("LeaderManager (Proxy):", leaderManagerProxy);
+        console.log("IncentiveManager (Implementation):", address(incentiveManagerImpl));
+        console.log("IncentiveManager (Proxy):", incentiveManagerProxy);
+    }
+
+    // Helper function to deploy a proxy
+    function deployProxy(address implementation, bytes memory data) internal returns (address) {
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            implementation,
+            address(proxyAdmin),
+            data
+        );
+        return address(proxy);
     }
 }
